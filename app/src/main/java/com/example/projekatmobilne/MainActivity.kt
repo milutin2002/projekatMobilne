60caaa7744f88a1fdecca93cb3438163578cb2a2
milutin2002 <milutinjovanovic2002@gmail.com>
2024-07-07 11:13:12 +0200

    Dodao sam da se prikazuju item stvari

--- app/src/main/java/com/example/projekatmobilne/MainActivity.kt
+++ app/src/main/java/com/example/projekatmobilne/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.projekatmobilne
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,9 +13,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -25,6 +33,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
@@ -68,7 +78,9 @@ fun shoppingMain() {
                 .padding(16.dp)
         ) {
             items(shopItems) {
-
+                ShoppingItemShow(item = it, onEditClick = {  }, onDeleteClick = {
+                    
+                })
             }
         }
     }
@@ -109,6 +121,22 @@ fun shoppingMain() {
                 )
             }
         })
+    }
+}
+@Composable
+fun ShoppingItemShow(item:ShoppingItem,onEditClick:()->Unit,onDeleteClick:()->Unit){
+    Row(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth()
+        .border(border = BorderStroke(2.dp, Color(0xFF018786)), shape = RoundedCornerShape(20))){
+        Text(text = item.name)
+        Text(text = item.quantity.toString())
+        IconButton(onClick =  onEditClick) {
+            Icon(imageVector = Icons.Default.Edit,contentDescription = null)
+        }
+        IconButton(onClick =  onDeleteClick) {
+            Icon(imageVector = Icons.Default.Delete,contentDescription = null)
+        }
     }
 }
 @Composable
