cfbf110e5ec1429111a33206dc889be0dade09b1
milutin2002 <milutinjovanovic2002@gmail.com>
2024-09-05 19:15:52 +0200

    Final commit

--- app/src/main/java/com/example/projekatmobilne/ui/theme/screens/LocationSelectData.kt
+++ app/src/main/java/com/example/projekatmobilne/ui/theme/screens/LocationSelectData.kt
@@ -1,5 +1,7 @@
 package com.example.projekatmobilne.ui.theme.screens
 
+import android.annotation.SuppressLint
+import android.os.Looper
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,6 +16,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.example.projekatmobilne.Repository.PlaceRepository
@@ -21,6 +24,10 @@ import com.example.projekatmobilne.RetrofitPackage.RetrofitInstance
 import com.example.projekatmobilne.model.LocationData
 import com.example.projekatmobilne.model.Place
 import com.example.projekatmobilne.viewModels.LocationViewModel
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -29,16 +36,19 @@ import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 
+@SuppressLint("MissingPermission")
 @Composable
-fun LocationSelectScreen(location:LocationData,onLocationSelected:(LocationData)->Unit){
+fun LocationSelectScreen(viewModel: LocationViewModel,location:LocationData,onLocationSelected:(LocationData)->Unit){
     val userLocation = remember { mutableStateOf(LatLng(location.latitude, location.longitude)) }
     val shopLocation = remember { mutableStateOf(LatLng(location.latitude,location.longitude)) }
+    val zoom= rememberSaveable{ mutableStateOf(15f) }
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(userLocation.value, 15f)
+        position = CameraPosition.fromLatLngZoom(userLocation.value, zoom.value)
     }
+    val context = LocalContext.current
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
     val selectedLocation= remember { mutableStateOf("") }
     val markerState = remember { MarkerState(position = userLocation.value) }
-    val shopMarkerState= remember {MarkerState(position = shopLocation.value)}
     var nearbyPlaces by remember { mutableStateOf<List<Place>?>(null) }
     var radiusInMeters by rememberSaveable { mutableStateOf(1) }
 
@@ -49,7 +59,28 @@ fun LocationSelectScreen(location:LocationData,onLocationSelected:(LocationData)
         nearbyPlaces = placeRepository.fetchClosestPlaces(locationString, radiusInMeters, "shopping_mall", "")
     }
 
+    val locationCallback = remember {
+        object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                val newLocation = locationResult.lastLocation
+                newLocation?.let {
+                    val newLatLng = LatLng(it.latitude, it.longitude)
+                    userLocation.value = newLatLng
+                    markerState.position = newLatLng
+
+                }
+            }
+        }
+    }
+
     LaunchedEffect(Unit) {
+        val locationRequest = LocationRequest.create().apply {
+            interval = 5000
+            fastestInterval = 2000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
         fetchNearbyPlaces(userLocation.value)
     }
 
@@ -62,11 +93,9 @@ fun LocationSelectScreen(location:LocationData,onLocationSelected:(LocationData)
             modifier = Modifier
                 .weight(1f)
                 .padding(top = 16.dp),
-            cameraPositionState = cameraPositionState,
+            cameraPositionState = cameraPositionState
         ) {
             Marker(state = markerState)
-            Marker(state = shopMarkerState)
-            // Display markers for all nearby places
             nearbyPlaces?.forEach { place ->
                 Marker(
                     state = rememberMarkerState(position = LatLng(place.geometry.location.lat, place.geometry.location.lng)),
